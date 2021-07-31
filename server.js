072862aba241481bb79b44f72d3435e373f8b1de
Md Atif Ansar <atifansar002@gmail.com>
2021-07-31 22:56:52 +0530

    first commit

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ if(process.env.NODE_ENV == "production"){
     app.use(express.static("quoteblog/build"));
 }
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port:`+PORT);
 });
